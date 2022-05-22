71be3ae1ceb6b8aeebed24bd5bfea81de39c4cea
Alexander Makarov <Joker124rus@gmail.com>
2022-05-23 01:36:16 +0700

    Add saving data

--- Assets/Scripts/MoldText.cs
+++ Assets/Scripts/MoldText.cs
@@ -6,32 +6,79 @@ using UnityEngine.UI;
 public class MoldText : MonoBehaviour
 {
     private Text text;
-    private int count;
+    private const string keySavedMold = "SavedMold";
+    private int moldCount;
     private int addCountPassive = 1;
     private int addPassiveInterval = 50;
     private int timer;
+
+    void Awake()
+    {
+        LoadGame();
+    }
     
     void Start()
     {
         text = GameObject.Find("Canvas/Text").GetComponent<Text>();
-        text.text = "Плесень " + count;
+        text.text = "Плесень " + moldCount;
     }
     
     void FixedUpdate()
     {
         if (timer == addPassiveInterval)
         {
-            count += addCountPassive;
-            text.text = "Плесень " + count;
+            moldCount += addCountPassive;
+            text.text = "Плесень " + moldCount;
             timer = 0;
         }
 
         timer++;
     }
 
+    void OnDestroy()
+    {
+        SaveGame();
+    }
+
     public void AddMold(int addCount)
     {
-        count += addCount;
-        text.text = "Плесень " + count;
+        moldCount += addCount;
+        text.text = "Плесень " + moldCount;
     }
+
+
+    void SaveGame()
+    {
+        PlayerPrefs.SetInt(keySavedMold, moldCount);
+        PlayerPrefs.Save();
+        Debug.Log("Game data saved!");
+    }
+
+    void LoadGame()
+    {
+        if (PlayerPrefs.HasKey(keySavedMold))
+        {
+            moldCount = PlayerPrefs.GetInt(keySavedMold);
+            Debug.Log("Game data loaded!");
+        }
+        else
+        {
+            Debug.LogError("There is no save data!");
+        }
+    }
+
+    void ResetData()
+    {
+        PlayerPrefs.DeleteAll();
+        moldCount = 0;
+        Debug.Log("Data reset complete");
+    }
+
+    //public void OnApplicationPause(bool pauseStatus)
+    //{
+    //    if (pauseStatus)
+    //    {
+    //        SaveGame();
+    //    }
+    //}
 }
