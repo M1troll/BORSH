f2d689205a7bea8d5201325a97f287c16183fa83
M1troll <oboleninoff.anton@yandex.ru>
2022-05-30 18:14:27 +0700

    Create animations prefabs

---
+++ Assets/Scripts/FallingIce.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingIce : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 _startPoint = new Vector3(0.5748f, 5.8228f);
+
+    [SerializeField]
+    Vector3 _endPoint = new Vector3(0.67f, -1.07f);
+
+    [SerializeField]
+    float _speed = 1.4f;
+
+    private IEnumerator MovingCoroutine(Vector3 startPosition, Vector3 goalPosition)
+    {
+        float wTimer = 0;
+
+        while (transform.position != goalPosition)
+        {
+            yield return transform.position = Vector3.Lerp(startPosition, goalPosition, wTimer);
+            wTimer += Time.deltaTime * _speed;
+        }
+    }
+
+    [ContextMenu("Как падать?")]
+    public void DropDownIceberg()
+    {
+        transform.position = _startPoint;
+        StartCoroutine(MovingCoroutine(_startPoint, _endPoint));
+    }
+
+    public void DropDownIceberg(Vector3 start, Vector3 end, float speed = 1.4f)
+    {
+        _speed = speed;
+        transform.position = start;
+        StartCoroutine(MovingCoroutine(start, end));
+    }
+}
---
+++ Assets/Scripts/FlyMove.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FlyMove : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 _endPoint = new Vector3(4.2f, 4.8f);
+
+    [SerializeField]
+    Vector3 _startPoint = new Vector3(-4.5f, -3.71f);
+
+    [SerializeField]
+    float _speed = 0.3f;
+
+
+    private IEnumerator MovingCoroutine(Vector3 startPosition, Vector3 goalPosition)
+    {
+        float wTimer = 0;
+
+        while (transform.position != goalPosition)
+        {
+            yield return transform.position = Vector3.Lerp(startPosition, goalPosition, wTimer);
+            wTimer += Time.deltaTime * _speed;
+        }
+    }
+
+    [ContextMenu("Как какать")]
+    public void StartFlyMove()
+    {
+        transform.position = _startPoint;
+        StartCoroutine(MovingCoroutine(_startPoint, _endPoint));
+    }
+
+    public void StartFlyMove(Vector3 start, Vector3 end, float speed = 0.3f)
+    {
+        _speed = speed;
+        transform.position = start;
+        StartCoroutine(MovingCoroutine(start, end));
+    }
+
+}
